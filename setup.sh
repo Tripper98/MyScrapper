d3485ca9d650b7be15b0cdaedc350701955fb61e
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2021-08-28 19:04:20 +0100

    del streamlit folder

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "[theme]
-primaryColor = ‘#84a3a7’
-backgroundColor = ‘#EFEDE8’
-secondaryBackgroundColor = ‘#fafafa’
-textColor= ‘#424242’
-font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[theme]\n\
+primaryColor = ‘#F63366’\n\
+backgroundColor = ‘#fef8ef’\n\
+secondaryBackgroundColor = ‘#ebd2b9’\n\
+textColor= ‘#262730’\n\
+font = ‘serif’\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\ 
 " > ~/.streamlit/config.toml
