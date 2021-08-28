112adf3dc0ee9c2d6316bc7c0fa85eb735bbea78
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2021-08-28 18:48:15 +0100

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]\n\
+primaryColor= "#F63366"\n\
+backgroundColor="#fef8ef"\n\
+secondaryBackgroundColor="#ebd2b9"\n\
+textColor="#262730"\n\
+font="serif"\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
