39adbc662b4d177dcb1b443c36de7d08de671383
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2021-08-29 00:17:59 +0100

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]\n\
-primaryColor = ‘#F63366’\n\
-backgroundColor = ‘#fef8ef’\n\
-secondaryBackgroundColor = ‘#ebd2b9’\n\
-textColor= ‘#262730’\n\
-font = ‘serif’\n\
 [server]\n\
-headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-\n\ 
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
